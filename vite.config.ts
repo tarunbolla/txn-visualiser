05f549a5cca9055d467cb71914988daa61e10c88
Tarun Bolla <tarunkumar.bolla@gmail.com>
2025-07-06 20:30:30 +1000

    fix: update project base path to 'txn-visualiser' and remove all references to 'txn-visualiser-react'

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/txn-visualiser-react/', // <-- Set to your repo name for GitHub Pages
+  base: '/txn-visualiser/', // <-- Set to your repo name for GitHub Pages
   plugins: [react()],
 })
